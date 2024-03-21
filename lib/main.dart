b1913339fefa863bd138794aeb4c73439c9cafc5
ariyha <nithishariyha02467@gmail.com>
2024-03-21 23:05:15 +0530

    Basic layout for home page

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Diary",
+    home: home(),
+  ));
+}
+
+class home extends StatelessWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final x = <Widget>[];
+    for (int i = 0; i <= 30; i++) {
+      x.add(record());
+    }
+    return Scaffold(
+        backgroundColor: ColorScheme.dark().background,
+        appBar: AppBar(
+          backgroundColor: ColorScheme.dark().primaryContainer,
+          title: const Text(
+            "Diary",
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search_outlined),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('This is a snackbar')));
+              },
+            )
+          ],
+        ),
+        body: ListView(children: x),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              final bar = SnackBar(content: Text("Fuck this shitttt"));
+              ScaffoldMessenger.of(context).showSnackBar(bar);
+            }));
+  }
+}
+
+class record extends StatelessWidget {
+  const record({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return SizedBox(
+        height: size.height * 0.1,
+        width: size.width,
+        child: const Card(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Flexible(
+                  flex: 7,
+                  fit: FlexFit.tight,
+                  child: Card(
+                    child: Text("text"),
+                  )),
+              Flexible(
+                  flex: 3,
+                  fit: FlexFit.tight,
+                  child: Card(
+                    child: Text("Date"),
+                  ))
+            ],
+          ),
+        ));
+  }
+}
